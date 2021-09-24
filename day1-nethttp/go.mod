3cbfa115168405412f87c068ac7932762fef9992
touch <396223353@qq.com>
2021-09-24 18:06:40 +0800

    搭建和练习

---
+++ day1-nethttp/go.mod
@@ -0,0 +1,3 @@
+module touch.github.com/goweb/day1-nethttp
+
+go 1.17
---
+++ day1-nethttp/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	// "strings"
+)
+
+const (
+	mutexLocked   = 1 << iota // 1
+	mutexWoken                // 2
+	mutexStarving             // 4
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+}
